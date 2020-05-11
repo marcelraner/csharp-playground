9f6ae91829230a4e79d61132c2b5b6d201013d59
Marcel Raner <marcel.raner@gmail.com>
2020-05-11 16:38:54 +0200

    Add timestamp sample

---
+++ 02-timestamp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _02_timestamp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime time = DateTime.UtcNow;
+            String timeString = time.ToString("yyyy-MM-dd HH:mm:ss.fff");
+            Console.WriteLine(timeString);
+        }
+    }
+}
---
+++ 02-timestamp/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+dotnet run --project ./02-timestamp.csproj
